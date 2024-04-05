81e8a493be1d966581e92272d5163331f9c47afa
Juan Martinez <juanmartinez5678@hotmail.com>
2024-04-05 15:45:34 -0400

    week 1 notes

---
+++ intro-to-software-engineering/week1.py
@@ -0,0 +1,11 @@
+# SDLC is a scientific approach to develop software, it also guides the development process. identifies discrete steps needed to develop software
+# SDLC is a process that produces software with the highest quality and lowest cost in the shortest time. SDLC includes a detailed plan for how to develop, alter, maintain, and replace a software system.
+# SDLC began in the 1960s to develop large scale functional business systems in the government and industries. SDLC is a process followed for a software project, within a software organization. It consists of a detailed plan describing how to develop, maintain, replace and alter or enhance specifi
+
+# there are 6 phases in every SDLC, they are:
+# 1. Planning - gatthered, analyzed, documented, and prioritized, SRS is created
+# 2. Design - requieremnts are gathered from the SRS to develop the architecture , a prototype might be developed
+# 3. Development - the actual code is written and the software is developed
+# 4. Testing - code is tested to ensure stability, security , and that it meets the requirements from the SRS
+# 5. Deployment
+# 6. Maintenance
