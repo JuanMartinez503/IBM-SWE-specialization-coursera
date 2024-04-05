28980c6c9393a4f5558d650e7a5f53df6c06874d
Juan Martinez <juanmartinez5678@hotmail.com>
2024-04-05 16:52:06 -0400

    added week 2 notes

---
+++ intro-to-software-engineering/week2.js
@@ -0,0 +1,6 @@
+// CI/CD 
+// Continuous Integration and Continuous Deployment
+// Enables developers to deliver frequent changes reliably and quickly
+// implement through a build-aitomation server
+// CI automatically builds and tests code changes
+// CD automatically deploys code changes to a testing or production environment
